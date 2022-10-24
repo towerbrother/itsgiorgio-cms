50372c09ef22cf156f7f2d8d183d4ddfe9586bb0
GTO <giorgio.torre@stellenanzeigen.de>
2022-10-24 12:36:40 +0200

    removed unused legalfooter and modified footer

--- schemas/footer.js
+++ schemas/footer.js
@@ -2,5 +2,8 @@ export const footer = {
   name: 'footer',
   title: 'Footer',
   type: 'document',
-  fields: [{ name: 'title', title: 'Title', type: 'string' }],
+  fields: [
+    { name: 'title', title: 'Title', type: 'string' },
+    { name: 'copyright', title: 'Copyright', type: 'string' },
+  ],
 };
--- schemas/legalFooter.js
+++
@@ -1,6 +0,0 @@
-export const legalFooter = {
-  name: 'legalFooter',
-  title: 'LegalFooter',
-  type: 'document',
-  fields: [{ name: 'title', title: 'Title', type: 'string' }],
-};
